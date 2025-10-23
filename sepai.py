86c323eb20f7d5a4a43e610e8ec1a83434922ba8
david <datwell845@gmail.com>
2025-10-23 01:57:59 -0500

    Add content to core project files

---
+++ sepai.py
@@ -0,0 +1,49 @@
+ 
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+SepAI - AI-Powered Color Separation for GIMP
+Copyright (C) 2025 SepAI Contributors
+Licensed under GPL v3
+
+Intelligent screen printing color separation using Google Gemini AI.
+"""
+
+__version__ = '0.1.0'
+__author__ = 'SepAI Contributors'
+__license__ = 'GPL v3'
+
+# TODO: Implement GIMP plugin
+# This is a placeholder for development
+
+if __name__ == "__main__":
+    print("SepAI v0.1.0 - Development")
+    print("This will become a GIMP plugin")
+```
+
+4. Save: `Ctrl + S`
+
+---
+
+### **Step 5: Add GPL v3 License**
+
+1. Click on `LICENSE` in VS Code
+2. Paste this:
+```
+GNU GENERAL PUBLIC LICENSE
+Version 3, 29 June 2007
+
+Copyright (C) 2025 SepAI Contributors
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
